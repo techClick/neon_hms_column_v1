b09eb0dd12a1c62a9a1a6509a840aaa070a7b12f
Ikechi Anya <ikechi@agures.com>
2023-10-24 17:03:12 +0100

    bug fix 1

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json());
-['clients'].map((endPoint) => app.use('/', require(`./routes/${endPoint}`)))
+app.use(express.json())
+// ['clients', 'auth'].map((endPoint) => app.use('/', require(`./routes/${endPoint}`)))
 require('dotenv').config()
 
 const port = process.env.PORT || 8000
